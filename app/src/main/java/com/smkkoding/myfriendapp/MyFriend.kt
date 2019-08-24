72319ae55138194c651db964b0c3bc8170d1f170
adipe007 <adiprasetio007@gmail.com>
2019-08-24 16:03:45 +0700

    Initial commit

---
+++ app/src/main/java/com/smkkoding/myfriendapp/MyFriend.kt
@@ -0,0 +1,9 @@
+package com.smkkoding.myfriendapp
+
+data class MyFriend(
+            val nama: String,
+            val kelamin: String,
+            val email: String,
+            val telp: String,
+            val alamat: String
+)
